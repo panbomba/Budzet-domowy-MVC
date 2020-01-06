352426a1f3ecb77d9d422e87ec77c85cb99d55d0
Maciej Bombik <maciej.bombik.programista@gmail.com>
2020-01-06 22:41:29 +0100

    bilans zaczyna dzialac - dodac pie chart

--- php-mvc/App/Controllers/Balance.php
+++ php-mvc/App/Controllers/Balance.php
@@ -4,10 +4,9 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Models\User;
-//prawdopodobnie rowniez pozostale modele
-
-use \App\Auth;
-use \App\Flash;
+use \App\Models\ExpenseTransaction;
+use \App\Models\IncomeTransaction;
+use \App\Models\BalanceModel;
 
 class Balance extends \Core\Controller
 {
@@ -15,4 +14,92 @@ class Balance extends \Core\Controller
 	{
 		View::renderTemplate('Balance/balance.html');
 	}		
-}
+	
+		public function createAction()
+	{
+		if($_POST['okres'] == 1)
+		{
+			$_SESSION['data_poczatkowa'] = date("Y-m-d", strtotime("first day of this month"));
+			$_SESSION['data_koncowa'] = date("Y-m-d", strtotime("today"));
+		}
+		else if($_POST['okres'] == 2)
+		{
+			$_SESSION['data_poczatkowa'] = date("Y-m-d", strtotime("first day of last month"));
+			$_SESSION['data_koncowa'] = 	date("Y-m-d", strtotime("last day of last month"));				
+		}
+		else if($_POST['okres'] == 3)
+		{
+			$_SESSION['data_poczatkowa'] = date("Y-m-d", strtotime("first day of January"));
+			$_SESSION['data_koncowa'] = 	date("Y-m-d", strtotime("today"));			
+		}
+		else if($_POST['okres'] == 4)
+		{
+			$poczatek = $_POST['start'];
+			$koniec = $_POST['end'];
+			$_SESSION['data_poczatkowa'] = $poczatek;
+			$_SESSION['data_koncowa'] = $koniec;
+		}
+		
+		$tablica4 = BalanceModel::getSumOfExpenses($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
+		$tablica5 = BalanceModel::getSumOfIncomes($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
+		$_SESSION['suma_wydatkow'] = -(double)$tablica4['SUM(amount)'];
+		$_SESSION['suma_przychodow'] = (double)$tablica5['SUM(amount)'];
+		$_SESSION['bilans'] = ($_SESSION['suma_przychodow'] + $_SESSION['suma_wydatkow']); 
+	
+		View::renderTemplate('Balance/balance.html');		
+	}
+		
+		// przekazanie ustawionych dat do modelu ktory nastepnie zwroci dane do wyswietlenia
+		//powrot do strony bilansu
+		// czy zapisywac dane do sesji?
+		public static function getStartDate()
+		{
+			if(isset($_SESSION['data_poczatkowa']))
+			{
+				return $_SESSION['data_poczatkowa'];
+			}
+			else 
+			{
+				$_SESSION['data_poczatkowa'] = date("Y-m-d", strtotime("today"));
+				return $_SESSION['data_poczatkowa'];
+			}				
+		}
+		public static function getEndDate()
+		{
+			if(isset($_SESSION['data_koncowa']))
+			{
+				return $_SESSION['data_koncowa'];
+			}
+			else 
+			{
+				$_SESSION['data_koncowa'] = date("Y-m-d", strtotime("first day of this month"));
+				return $_SESSION['data_koncowa'];
+			}		
+		}
+		public static function showSumOfExpenses()		
+		{
+			if(isset($_SESSION['suma_wydatkow']))
+			{
+				return $_SESSION['suma_wydatkow'];
+			}
+		}
+		public static function showSumOfIncomes()		
+		{
+			if(isset($_SESSION['suma_przychodow']))
+			{
+				return $_SESSION['suma_przychodow'];
+			}			
+			else
+			{
+				//$tablica6 = BalanceModel::getSumOfIncomes($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);	
+				//$_SESSION['suma_przychodow'] = (double)$tablica6['SUM(amount)'];			
+			}
+		}	
+		public static function getBalance()
+		{
+			if(isset($_SESSION['bilans']))
+			{
+				return $_SESSION['bilans'];
+			}					
+		}
+	}		
---
+++ php-mvc/App/Models/BalanceModel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+use \Core\View;
+
+use PDO;
+
+/**
+ * Example user model
+ *
+ * PHP version 7.0
+ */
+class BalanceModel extends \Core\Model
+{
+	public static function getSumOfExpenses($start, $end)
+	{
+		$user_id = $_SESSION['user_id'];
+		$data_poczatkowa = date($start);
+		$data_koncowa = date($end);
+		
+		$sql = "SELECT SUM(amount) FROM expenses WHERE date_of_expense BETWEEN '$data_poczatkowa' AND '$data_koncowa' AND user_id = '$user_id'";
+		
+		$db = static::getDB();
+		$stmt = $db->prepare($sql);
+
+		$stmt->execute();		
+		return $stmt->fetch();	
+	}
+	
+	public static function getSumOfIncomes($start, $end)
+	{
+		$data_poczatkowa = date($start);
+		$data_koncowa = date($end);		
+		$user_id = $_SESSION['user_id'];
+
+		$sql = "SELECT SUM(amount) FROM incomes WHERE date_of_income BETWEEN '$data_poczatkowa' AND '$data_koncowa' AND user_id = '$user_id'";
+		
+		$db = static::getDB();
+		$stmt = $db->prepare($sql);
+
+		$stmt->execute();		
+		return $stmt->fetch();					
+	}
+	
+}
