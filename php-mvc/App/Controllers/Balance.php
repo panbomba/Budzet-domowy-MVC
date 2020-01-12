c655767db2786a7f023159761ea9f6439c918ab2
Maciej Bombik <maciej.bombik.programista@gmail.com>
2020-01-12 19:24:01 +0100

    usunieto niepotrzebne zmienne sesyjne

--- php-mvc/App/Controllers/Balance.php
+++ php-mvc/App/Controllers/Balance.php
@@ -18,23 +18,12 @@ class Balance extends \Core\Controller
 		$args['incomes'] = (double)$tablica5['SUM(amount)'];
 		$args['expenses'] = -(double)$tablica4['SUM(amount)'];
 		$args['balance'] = ((double)$tablica5['SUM(amount)'] + -(double)$tablica4['SUM(amount)']);
-		var_dump($args);
-		/*$_SESSION['data_poczatkowa'] = date("Y-m-d", strtotime("first day of this month"));
-		$_SESSION['data_koncowa'] = date("Y-m-d", strtotime("today"));
-
-		$tablica4 = BalanceModel::getSumOfExpenses($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-		$tablica5 = BalanceModel::getSumOfIncomes($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-		$_SESSION['suma_wydatkow'] = -(double)$tablica4['SUM(amount)'];
-		$_SESSION['suma_przychodow'] = (double)$tablica5['SUM(amount)'];
-		$_SESSION['bilans'] = ($_SESSION['suma_przychodow'] + $_SESSION['suma_wydatkow']); 
-		static::showSumOfExpenses();
-		static::showSumOfIncomes();
-		$przychody_kategorie = BalanceModel::getIncomeCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-		$_SESSION['przychody_tablica'] = $przychody_kategorie;
-		$wydatki_kategorie = BalanceModel::getExpenseCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-		$_SESSION['wydatki_tablica'] = $wydatki_kategorie;*/
+		$args['incomes_categories'] = BalanceModel::getIncomeCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
+		$args['expenses_categories'] = BalanceModel::getExpenseCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);		
 		
 		View::renderTemplate('Balance/balance.html', $args);
+		unset($_SESSION['data_poczatkowa']);
+		unset($_SESSION['data_koncowa']);
 	}		
 	
 		public function createAction()
@@ -52,7 +41,7 @@ class Balance extends \Core\Controller
 		else if($_POST['okres'] == 3)
 		{
 			$_SESSION['data_poczatkowa'] = date("Y-m-d", strtotime("first day of January"));
-			$_SESSION['data_koncowa'] = 	date("Y-m-d", strtotime("today"));			
+			$_SESSION['data_koncowa'] = date("Y-m-d", strtotime("today"));			
 		}
 		else if($_POST['okres'] == 4)
 		{
@@ -63,35 +52,29 @@ class Balance extends \Core\Controller
 		}
 		
 		$tablica4 = BalanceModel::getSumOfExpenses($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-		$tablica5 = BalanceModel::getSumOfIncomes($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-		$_SESSION['suma_wydatkow'] = -(double)$tablica4['SUM(amount)'];
-		$_SESSION['suma_przychodow'] = (double)$tablica5['SUM(amount)'];
-		//$_SESSION['bilans'] = ($_SESSION['suma_przychodow'] + $_SESSION['suma_wydatkow']); 
+		$tablica5 = BalanceModel::getSumOfIncomes($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);		
+		$args = [];
+		$args['incomes'] = (double)$tablica5['SUM(amount)'];
+		$args['expenses'] = -(double)$tablica4['SUM(amount)'];
+		$args['balance'] = ((double)$tablica5['SUM(amount)'] + -(double)$tablica4['SUM(amount)']);
+		$args['incomes_categories'] = BalanceModel::getIncomeCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
+		$args['expenses_categories'] = BalanceModel::getExpenseCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);				
 
-		$przychody_kategorie = BalanceModel::getIncomeCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-		$wydatki_kategorie = BalanceModel::getExpenseCategories($_SESSION['data_poczatkowa'], $_SESSION['data_koncowa']);
-
-		$_SESSION['przychody_tablica'] = $przychody_kategorie;
-		$_SESSION['wydatki_tablica'] = $wydatki_kategorie;
-		
-		$_SESSION['wydatki_pie_chart']=array(array('Kategoria','Kwota'));
-		foreach ($przychody_kategorie as $var)
-		{
-			//echo "\n", $var['name']. ' '. $var['SUM(amount)']. '<br>';
-			array_push($_SESSION['wydatki_pie_chart'],$var['name'],$var['SUM(amount)']);
-			
-		}
-		var_dump($_SESSION['wydatki_pie_chart']);
-		
-
-		View::renderTemplate('Balance/balance.html');		
+		View::renderTemplate('Balance/balance.html', $args);		
+		unset($_SESSION['data_poczatkowa']);
+		unset($_SESSION['data_koncowa']);		
 	}
 		public static function getStartDate()
 		{
 			if(isset($_SESSION['data_poczatkowa']))
 			{
 				return $_SESSION['data_poczatkowa'];
-			}		
+			}
+			else 
+			{
+				$_SESSION['data_poczatkowa'] = date("Y-m-d", strtotime("first day of this month"));
+				return $_SESSION['data_poczatkowa'];
+			}				
 		}
 		public static function getEndDate()
 		{
@@ -99,28 +82,13 @@ class Balance extends \Core\Controller
 			{
 				return $_SESSION['data_koncowa'];
 			}
-		}
-		/*public static function getBalance()
-		{
-			if(isset($_SESSION['bilans']))
+			else 
 			{
-				return $_SESSION['bilans'];
-			}					
-		}*/
-		public static function getIncomeTable()
-		{
-			if(isset($_SESSION['przychody_tablica']))
-			{
-				return $_SESSION['przychody_tablica'];
-			}					
-		}	
-		public static function getExpenseTable()
-		{
-			if(isset($_SESSION['wydatki_tablica']))
-			{
-				return $_SESSION['wydatki_tablica'];
+				$_SESSION['data_koncowa'] = date("Y-m-d", strtotime("today"));
+				return $_SESSION['data_koncowa'];
 			}					
 		}
+		
 		public static function getExpenseForPieChart()
 		{
 			if(isset($_SESSION['wydatki_pie_chart']))
--- php-mvc/Core/View.php
+++ php-mvc/Core/View.php
@@ -46,16 +46,10 @@ class View
         if ($twig === null) {
             $loader = new \Twig_Loader_Filesystem(dirname(__DIR__) . '/App/Views');
             $twig = new \Twig_Environment($loader);
-			//$twig->addGlobal('session', $_SESSION);
 			$twig->addGlobal('current_user', \App\Auth::getUser());
 			$twig->addGlobal('start_date', \App\Controllers\Balance::getStartDate());
 			$twig->addGlobal('end_date', \App\Controllers\Balance::getEndDate());
-			//$twig->addGlobal('sum_of_expenses', \App\Controllers\Balance::showSumOfExpenses());
-			//$twig->addGlobal('sum_of_incomes', \App\Controllers\Balance::showSumOfIncomes());
-			$twig->addGlobal('table_of_incomes', \App\Controllers\Balance::getIncomeTable());
-			$twig->addGlobal('table_of_expenses', \App\Controllers\Balance::getExpenseTable());
 			$twig->addGlobal('table_of_expenses_pie_chart', \App\Controllers\Balance::getExpenseForPieChart());
-			//$twig->addGlobal('bilans', \App\Controllers\Balance::getBalance());
 			$twig->addGlobal('flash_messages', \App\Flash::getMessages());
         }
 
