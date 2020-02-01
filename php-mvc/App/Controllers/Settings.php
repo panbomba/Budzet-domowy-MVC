1d9b71a1dac249fd6cc30d18098098d14fb5f6ed
Maciej Bombik <maciej.bombik.programista@gmail.com>
2020-02-01 23:14:03 +0100

    strona ustawien poprawiona

--- php-mvc/App/Controllers/Settings.php
+++ php-mvc/App/Controllers/Settings.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Models\User;
+use \App\Models\ExpenseTransaction;
+use \App\Models\IncomeTransaction;
 //prawdopodobnie rowniez pozostale modele
 
 use \App\Auth;
@@ -13,6 +15,10 @@ class Settings extends \Core\Controller
 {
 	public function newAction()
 	{
-		View::renderTemplate('Settings/settings.html');
+		$args = [];
+		$args['inc_cat_assigned'] = IncomeTransaction::getIncomeCategoriesAssignedToUser();				
+		$args['exp_cat_assigned'] = ExpenseTransaction::getExpenseCategoriesAssignedToUser();
+		$args['pay_meth_assigned'] = ExpenseTransaction::getPaymentMethodsAssignedToUser();		
+		View::renderTemplate('Settings/settings.html', $args);
 	}	
 }
